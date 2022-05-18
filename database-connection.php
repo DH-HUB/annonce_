b0a987349711c6d0ac2a8058b6fc9139011e7806
DH-HUB <60735833+DH-HUB@users.noreply.github.com>
2022-05-18 11:19:40 +0200

    Database update

--- database-connection.php
+++ database-connection.php
@@ -1,11 +1,11 @@
 <?php
 
 return new PDO(
-	'mysql:host=localhost;dbname=blogphp;charset=utf8',
+	'mysql:host=localhost;dbname=annonces;charset=utf8',
 	'root',
 	'',
 	[
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,git
 		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 	]
 );
